f8466dd22c3950580e046202ccbc49407b40b5da
Oscar Lawton <emmetlawton@gmail.com>
2021-06-15 01:30:43 +0100

    1:03:10

--- src/App.js
+++ src/App.js
@@ -29,11 +29,18 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
+  
+  // Toggle Reminder 
+  const toggleReminder = (id) => {
+    console.log()
+    setTasks(tasks.map((task) => task.id === id? { ...task, reminder: !task.reminder }: task))
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Taskas')}
+      
     </div>
   );
 } 
