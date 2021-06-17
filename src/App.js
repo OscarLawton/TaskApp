5915bde3ce0b54011fa0f74cdd605859976b9089
Oscar Lawton <emmetlawton@gmail.com>
2021-06-18 00:09:49 +0100

    1:14:34

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import React from 'react'
 import Header from './components/Header'
+import AddTask from './components/AddTask'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
 
@@ -25,6 +26,13 @@ function App() {
       }
   ])
   
+  // Add Task 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -39,6 +47,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Taskas')}
       
     </div>
