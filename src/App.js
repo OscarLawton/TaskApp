a46ecd3e4309693eae964395faa022b8e371dc3a
Oscar Lawton <emmetlawton@gmail.com>
2021-06-18 22:59:07 +0100

    toggle reminder @ 1:39:16

--- src/App.js
+++ src/App.js
@@ -18,8 +18,17 @@ function App() {
     getTasks()
   }, [])
 
+  // Fetch Tasks 
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
 
     return data
@@ -51,9 +60,21 @@ function App() {
   }
   
   // Toggle Reminder 
-  const toggleReminder = (id) => {
-    console.log()
-    setTasks(tasks.map((task) => task.id === id? { ...task, reminder: !task.reminder }: task))
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id? { ...task, reminder: data.reminder }: task))
   }
 
   return (
